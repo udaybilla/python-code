c19cac193b101bcaab279885f422a95754d2e1f2
Billa <ubilla2@dxc.com>
2019-08-09 15:18:44 -0500

    added new code to delete and take snapshot of the ebs vloumes

--- ebs_volumes.py
+++ ebs_volumes.py
@@ -1,4 +1,4 @@
-import boto3
+mport boto3
 import json
 
 ec2 = boto3.resource('ec2',region_name='us-east-1')
@@ -8,8 +8,24 @@ filters = [{
             'Values': ['available']
             }]
 filtered = base.filter(Filters=filters)
+
+myList=[]
 def lambda_handler(event, context):
-      for vol in filtered:
-              vol.delete(vol.id)
-                  print ("Deleting the orphaned EBS volumes" +vol.id)
-                      return vol.id
+      deleteEbsVolumes()
+        copySnapshots()
+          return myList
+        
+
+      def deleteEbsVolumes():
+            for vol in filtered:
+                    snap = vol.create_snapshot(vol.id)
+                        print ("Snapshot taken successfully")
+                            vol.delete(vol.id)
+                                print ("Deleting the orphaned EBS volumes" +vol.id)
+                                    myList.extend([vol.id,snap.id])
+                                       
+
+                                           
+                                           def copySnapshots():
+                                                  print (myList)
+
