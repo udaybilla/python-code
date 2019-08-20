41b4db452e950cb24c6c41f0c054038fe955e836
Billa <ubilla2@dxc.com>
2019-08-20 13:48:55 -0500

    mdoified the script

--- ebs_volumes.py
+++ ebs_volumes.py
@@ -2,6 +2,7 @@ import boto3
 import json
 
 ec2 = boto3.resource('ec2',region_name='us-east-1')
+client = boto3.client('ec2')
 base = ec2.volumes.all()
 filters = [{
         'Name': 'status',
@@ -9,20 +10,25 @@ filters = [{
             }]
 filtered = base.filter(Filters=filters)
 
-myList=[]
+myEbs=[]
+myEbsSnap=[]
+
 def lambda_handler(event, context):
       deleteEbsVolumes()
-      copySnapshots()
-      return myList
-        
+        copySnapshots()
+          return myEbs,myEbsSnap
 
-def deleteEbsVolumes():
+      def deleteEbsVolumes():
             for vol in filtered:
                     snap = vol.create_snapshot(vol.id)
                         print ("Snapshot taken successfully")
                             vol.delete(vol.id)
                                 print ("Deleting the orphaned EBS volumes" +vol.id)
-                                    myList.extend([vol.id,snap.id])
-                                        
-def copySnapshots()                                          
-  print (myList)
+                                    myEbs.append(vol.id)
+                                        myEbsSnap.append(snap.id)
+                                            
+                                            def copySnapshots():
+                                                   print (myEbsSnap,myEbs)
+                                                      for snapId in myEbsSnap:
+                                                               print (snapId)
+                                                                    response = client.copy_snapshot(SourceRegion='us-east-1', DestinationRegion='us-east-2', SourceSnapshotId=snapId, Description='Copy of ebs snapshots')
